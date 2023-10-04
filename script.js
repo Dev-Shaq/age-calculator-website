d785b43a303d45c25444a321444a64b2a4b3b0f9
Nweze Charles <104150471+EastBaby043@users.noreply.github.com>
2023-10-04 21:16:19 +0100

    updates css

---
+++ script.js
@@ -0,0 +1,20 @@
+// For Output
+const outputYear = document.querySelector('.output-year');
+const outputMonth = document.querySelector('.output-month');
+const outputDay = document.querySelector('.output-day');
+// for button
+const submitBtn = document.querySelector('submit-btn');
+
+// For Input
+const inputYear = document.querySelector('#year');
+const inputMonth = document.querySelector('#month');
+const inputDay = document.querySelector("#day");
+let isValid = false
+
+// For the error
+const errorYear= document.querySelector('error-year');
+const errorMonth= document.querySelector('error-month');
+const errorDay= document.querySelector("error-day");
+
+// adding event listener
+inputDay.addEventListenerq
